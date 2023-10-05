c502af2c5a314974f0a463cfc9b367dbe4c7330f
Raza Mahmood <razammahmood@gmail.com>
2023-10-05 17:40:31 -0400

    done Typewriter component

--- src/components/Typewriter.tsx
+++ src/components/Typewriter.tsx
@@ -4,23 +4,24 @@ type TypewriterProps = {
   value: string;
   timing: number;
   as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "span";
+  onCompleted?: () => void;
 } & React.HTMLAttributes<HTMLHeadingElement | HTMLSpanElement>;
 
-export const Typewriter = ({ value, timing, as = "span", ...props }: TypewriterProps) => {
+export const Typewriter = ({ value, timing, as = "span", onCompleted = () => {}, ...props }: TypewriterProps) => {
   const [text, setText] = useState("");
   const Element = as;
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setText(text + value.charAt(text.length));
-
+    const timer= setTimeout(() => {
       if (text.length === value.length) {
-        clearInterval(interval);
+        onCompleted();
+      } else {
+        setText(text + value.charAt(text.length));
       }
     }, timing);
 
-    return () => clearInterval(interval);
-  });
+    return () => clearTimeout(timer);
+  }, [text]);
 
   return <Element {...props} aria-label={value}>{text}</Element>;
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,26 @@
 import { Typewriter } from "../components/Typewriter";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const prompts = ["Welcome to Plant Time!", "What's your location?", "What plants do you want to grow?"];
+  const [prompt, setPrompt] = useState(0);
+
   return (
     <main className="flex flex-col">
-      <Typewriter value="Hello World!" timing={100} className="text-7xl p-6"/>
+      {prompts.map((value, index) => {
+        if (index < prompt) {
+          return <span key={value} className="text-7xl p-6">{value}</span>;
+        }
+      }).filter((value) => value)}
+      <Typewriter
+        value={prompts[prompt]}
+        key={prompts[prompt]}
+        timing={75}
+        className="text-7xl p-6"
+        onCompleted={() => {
+          setPrompt(Math.min(prompt + 1, prompts.length - 1));
+        }}
+      />
     </main>
   );
 }
