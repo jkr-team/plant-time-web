0f069a8c3d782586526b73e440444a21a5e86fd2
Raza Mahmood <razammahmood@gmail.com>
2023-10-13 13:37:42 -0400

    start working on multistepform again

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,22 +21,7 @@ export default function Home() {
       }}
     >
       <div className='lg:w-1/3 h-screen'>
-        <Chat>
-          <TextBubble type={'received'}>Welcome to Plant Time!</TextBubble>
-          <TextBubble type={'received'}>
-            We are here to help you decide what to plant in your garden!
-          </TextBubble>
-          <TextBubble type={'received'}>To do so we are going to need some information from you.</TextBubble>
-          <TextBubble type={'received'}>What's your location?</TextBubble>
-          <TextBubble type={'sent'}>6353 Juan Tabo Blvd NE, Apt 6, Albuquerque, New Mexico 87111</TextBubble>
-          <TextBubble type={'received'}>What's your soil type?</TextBubble>
-          <TextBubble type={'sent'}>Sandy</TextBubble>
-          <TextBubble type={'received'}>What's your soil pH?</TextBubble>
-          <TextBubble type={'sent'}>7.5</TextBubble>
-          <TextBubble type={'received'}>How much effort are you willing to expend?</TextBubble>
-          <TextBubble type={'sent'}>A lot</TextBubble>
-          <TypingIndicator />
-        </Chat>
+
       </div>
     </UserContext.Provider>
   );
