140a29e8651feb3905900a96bdc735b2114dffa9
Raza Mahmood <razammahmood@gmail.com>
2023-10-12 22:58:00 -0400

    allow for key other than index

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -4,14 +4,15 @@ import { TextBubbleProps } from './TextBubble';
 
 export interface ChatProps {
   children: ReactElement<TextBubbleProps>[];
+  key?: React.Key;
 }
 
-export const Chat = ({ children }: ChatProps) => {
+export const Chat = ({ key, children }: ChatProps) => {
   return (
     <div className='flex flex-col gap-2 w-full h-full overflow-y-auto'>
       {children.map((child, index) => (
         <div
-          key={index}
+          key={key ?? index}
           className={classNames('flex w-3/4', {
             'ml-auto': child.props.type === 'sent',
             'mr-auto': child.props.type === 'received',
