efd014d2273ef62b1973534016ce3d14f25fe564
Raza Mahmood <razammahmood@gmail.com>
2023-10-12 22:58:11 -0400

    change div to span

--- src/components/TypingIndicator.tsx
+++ src/components/TypingIndicator.tsx
@@ -5,9 +5,9 @@ export const TypingIndicator = () => {
   return (
     <TextBubble type={'received'}>
       <div className='flex items-center gap-2 select-none'>
-        <div className='animate-dim'>●</div>
-        <div className='animate-dim animation-delay-100'>●</div>
-        <div className='animate-dim animation-delay-200'>●</div>
+        <span className='animate-dim'>●</span>
+        <span className='animate-dim animation-delay-100'>●</span>
+        <span className='animate-dim animation-delay-200'>●</span>
       </div>
     </TextBubble>
   );
