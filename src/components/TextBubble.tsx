df28ece9ae1053c6902a7e4dbea0d6a7d932c44a
Raza Mahmood <razammahmood@gmail.com>
2023-10-12 23:09:53 -0400

    change color

--- src/components/TextBubble.tsx
+++ src/components/TextBubble.tsx
@@ -9,7 +9,7 @@ export interface TextBubbleProps {
 export const TextBubble = ({ type, children }: TextBubbleProps) => {
 
   return <div className={classNames('flex rounded-3xl text-xl py-2 px-4 w-fit max-w-full shadow-md animate-fade-in', {
-    'bg-blue-600 text-white dark:bg-blue-200 dark:text-black': type === 'sent',
-    'bg-zinc-200 text-black dark:bg-zinc-800 dark:text-white': type === 'received',
+    'bg-green-600 text-white dark:bg-green-200 dark:text-black': type === 'received',
+    'bg-zinc-200 text-black dark:bg-zinc-800 dark:text-white': type === 'sent',
   })}>{children}</div>;
 };
