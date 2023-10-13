4886b52666dea10995362828499d55c35b21b130
Raza Mahmood <razammahmood@gmail.com>
2023-10-12 22:47:00 -0400

    move user context to index.tsx and remove old code

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,50 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { Roboto_Condensed } from 'next/font/google';
-import { ThemeContext } from '../contexts/theme';
+import { Theme, ThemeContext } from '../contexts/theme';
 import { UserContext, type User } from '../contexts/user';
 import classNames from 'classnames';
+import { useMediaQuery } from '../hooks/useMediaQuery';
 
 const robotoCondensed = Roboto_Condensed({ subsets: ['latin'], weight: ['300', '400', '700'] });
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const [theme, setTheme] = useState('light' as 'light' | 'dark');
+  const [theme, setTheme] = useState('light' as Theme);
 
-  const [user, setUser] = useState({
-    location: { lat: NaN, lng: NaN },
-    soilType: '',
-    soilPH: NaN,
-  } as User);
-
-  const layoutClasses = classNames(
-    'flex-1 p-4 bg-sky-300 text-black transition-colors',
-    'dark:bg-sky-900 dark:text-white',
-    robotoCondensed.className,
-    theme
-  );
-
-  const mainClasses = classNames('flex flex-col mx-auto w-full max-w-container');
+  useEffect(() => {
+    // Set theme based on time of day
+    const hour = new Date().getHours();
+    setTheme(hour >= 6 && hour < 18 ? 'light' : 'dark');
+  }, []);
 
   return (
-    <ThemeContext.Provider
-      value={{
-        theme: theme,
-        toggleTheme: () => setTheme(theme === 'light' ? 'dark' : 'light'),
-        setTheme: (theme) => setTheme(theme),
-      }}
-    >
-      <UserContext.Provider
-        value={{
-          user: user,
-          updateLocation: (lat: number, lng: number) => setUser({ ...user, location: { lat, lng } }),
-          updateSoilType: (soilType: string) => setUser({ ...user, soilType }),
-          updateSoilPH: (soilPH: number) => setUser({ ...user, soilPH }),
-        }}
-      >
-        <div className={layoutClasses}>
-          <main className={mainClasses}>{children}</main>
+    <ThemeContext.Provider value={{ theme: theme }}>
+      <div className={`${theme} contents`}>
+        <div
+          className={classNames(
+            'flex-1 p-4 bg-sky-300 text-black transition-colors dark:bg-sky-900 dark:text-white',
+            robotoCondensed.className
+          )}
+        >
+          <main className='flex flex-col mx-auto w-full max-w-container'>{children}</main>
         </div>
-      </UserContext.Provider>
+      </div>
     </ThemeContext.Provider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,75 +1,38 @@
-import { MultiStepForm } from '../components/MultiStepForm';
-import { Typewriter } from '../components/Typewriter';
-import { type FormStep } from '../components/MultiStepForm';
-import { TextInput } from '../components/TextInput';
 import { TextBubble } from '../components/TextBubble';
 import { Chat } from '../components/Chat';
-
-const steps = [
-  {
-    title: "Welcome to Plant Time! \n What's your location?",
-    component: ({ onSubmit, submit }) => {
-      return (
-        <TextInput
-          type='text'
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              console.log('submitted location');
-              //onSubmit();
-            }
-          }}
-        />
-      );
-    },
-  },
-  {
-    title: 'What kind of soil do you have?',
-    component: ({ onSubmit }) => {
-      return (
-        <input
-          type='text'
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              console.log('submitted soil');
-              //onSubmit();
-            }
-          }}
-        ></input>
-      );
-    },
-  },
-  {
-    title: 'How stinky are you?',
-    component: ({ onSubmit }) => {
-      return (
-        <input
-          type='text'
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              console.log('submitted soil');
-              //onSubmit();
-            }
-          }}
-        ></input>
-      );
-    },
-  },
-] as FormStep[];
+import { useState } from 'react';
+import { User, UserContext } from '../contexts/user';
+import { TypingIndicator } from '../components/TypingIndicator';
 
 export default function Home() {
+  const [user, setUser] = useState({
+    location: { lat: NaN, lng: NaN },
+    soilType: '',
+    soilPH: NaN,
+  } as User);
+
   return (
-    <div className="lg:w-1/3 h-screen">
-      <Chat>
-        <TextBubble type={'received'}>Welcome to Plant Time!</TextBubble>
-        <TextBubble type={'received'}>A app that helps you decide what you should plant in your garden today!</TextBubble>
-        <TextBubble type={'received'}>To do so we are going to need some information from you.</TextBubble>
-        <TextBubble type={'received'}>What's your location?</TextBubble>
-        <TextBubble type={'sent'}>6353 Juan Tabo Blvd NE, Apt 6, Albuquerque, New Mexico 87111</TextBubble>
-        <TextBubble type={'received'}>ok i pull up</TextBubble>
-        <TextBubble type={'received'}>hop out to the after party</TextBubble>
-        <TextBubble type={'received'}>you and all your friends</TextBubble>
-        <TextBubble type={'received'}>yeah you love to get naughty</TextBubble>
-      </Chat>
-    </div>
+    <UserContext.Provider
+      value={{
+        user: user,
+        updateLocation: (lat: number, lng: number) => setUser({ ...user, location: { lat, lng } }),
+        updateSoilType: (soilType: string) => setUser({ ...user, soilType }),
+        updateSoilPH: (soilPH: number) => setUser({ ...user, soilPH }),
+      }}
+    >
+      <div className='lg:w-1/3 h-screen'>
+        <Chat>
+          <TextBubble type={'received'}>Welcome to Plant Time!</TextBubble>
+          <TextBubble type={'received'}>
+            A app that helps you decide what you should plant in your garden today!
+          </TextBubble>
+          <TextBubble type={'received'}>To do so we are going to need some information from you.</TextBubble>
+          <TextBubble type={'received'}>What's your location?</TextBubble>
+          <TextBubble type={'sent'}>6353 Juan Tabo Blvd NE, Apt 6, Albuquerque, New Mexico 87111</TextBubble>
+          <TextBubble type={'received'}>ok i pull up</TextBubble>
+          <TypingIndicator />
+        </Chat>
+      </div>
+    </UserContext.Provider>
   );
 }
