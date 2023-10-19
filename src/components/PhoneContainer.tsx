eaca8448098289fcbf6fd83a101719a76e769395
Raza Mahmood <razammahmood@gmail.com>
2023-10-19 18:33:06 -0400

    different shadow on PhoneContainer in dark theme

--- src/components/PhoneContainer.tsx
+++ src/components/PhoneContainer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { DigitalClock } from './DigitalClock';
 
 export const PhoneContainer = ({ children }: { children: React.ReactNode }) => (
-  <div className='relative flex w-full flex-1 flex-col overflow-hidden shadow-2xl md:max-h-[1200px] md:max-w-[568px] md:rounded-3xl'>
+  <div className='relative flex w-full flex-1 flex-col overflow-hidden shadow-2xl dark:shadow-xl md:max-h-[1200px] md:max-w-[568px] md:rounded-3xl'>
     <div className="flex py-2 px-4 items-center justify-between bg-zinc-100 text-lg dark:bg-zinc-800 z-20">
       <span>PLANTTIME</span>
       <DigitalClock />
