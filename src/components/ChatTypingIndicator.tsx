bdbc88fd791005f4fdf528254993198f318b81e1
Raza Mahmood <razammahmood@gmail.com>
2023-10-18 22:52:17 -0400

    use tailwind-animate plugin

--- src/components/ChatTypingIndicator.tsx
+++ src/components/ChatTypingIndicator.tsx
@@ -6,8 +6,8 @@ export const ChatTypingIndicator = () => {
     <ChatBubble type={'received'}>
       <div className='flex select-none items-center gap-1'>
         <span className='animate-dim'>●</span>
-        <span className='animate-dim animation-delay-100'>●</span>
-        <span className='animate-dim animation-delay-200'>●</span>
+        <span className='animate-dim delay-100'>●</span>
+        <span className='animate-dim delay-300'>●</span>
       </div>
     </ChatBubble>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from 'tailwindcss';
-import plugin from 'tailwindcss/plugin';
 
 const config: Config = {
   darkMode: 'class',
@@ -15,63 +14,25 @@ const config: Config = {
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       keyframes: {
-        blink: {
-          '50%': { opacity: '0' },
-        },
-        fade: {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' },
-        },
         dim: {
           '0%': { opacity: '0.25' },
           '100%': { opacity: '1' },
         },
-        flyInY: {
-          '0%': { transform: 'translateY(100%)' },
-          '100%': { transform: 'translateY(0)' },
-        },
       },
       animation: {
-        blink: 'blink 1s step-start infinite',
-        'fade-in': 'fade 0.25s ease-in-out',
         dim: 'dim 0.5s ease-in-out infinite alternate',
-        'fly-in-y': 'flyInY .75s ease-in-out',
       },
       colors: {
         sky: {
-          '50': '#f0f8ff',
-          '100': '#e1f0fd',
-          '200': '#bbe2fc',
-          '300': '#87cefa',
-          '400': '#3cb1f4',
-          '500': '#1296e5',
-          '600': '#0678c3',
-          '700': '#065f9e',
-          '800': '#095183',
-          '900': '#0e446c',
-          '950': '#092b48',
+          day: '#87cefa',
+          night: '#201F27',
         },
       },
       maxWidth: {
-        container: '1366px',
+        container: '1400px',
       },
     },
   },
-  plugins: [
-    plugin(({ matchUtilities, theme }) => {
-      matchUtilities(
-        {
-          'animation-delay': (value) => {
-            return {
-              'animation-delay': value,
-            };
-          },
-        },
-        {
-          values: theme('transitionDelay'),
-        }
-      );
-    }),
-  ],
+  plugins: [require('tailwindcss-animate')],
 };
 export default config;
