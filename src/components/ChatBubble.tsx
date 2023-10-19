7fc2aee24f1d2093e5aaf552d32c8b3a4870b78d
Raza Mahmood <razammahmood@gmail.com>
2023-10-19 13:40:55 -0400

    fixed escape error in ChatBubble

--- src/components/ChatBubble.tsx
+++ src/components/ChatBubble.tsx
@@ -10,7 +10,7 @@ export const ChatBubble = ({ type, children }: TextBubbleProps) => {
   return (
     <div
       className={classNames(
-        "m-1.5 flex w-fit max-w-full rounded-3xl px-4 py-2 text-xl shadow-md before:block before:content-['\200B']",
+        "m-1.5 flex w-fit max-w-full rounded-3xl px-4 py-2 text-xl shadow-md before:block before:content-['\\200B']",
         {
           'bg-zinc-200 text-black dark:bg-zinc-700 dark:text-white': type === 'received',
           'bg-blue-500 text-white dark:bg-blue-600': type === 'sent',
