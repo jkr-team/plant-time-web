4612c06829e3db9b952845f16b1469e04d31071f
Raza Mahmood <razammahmood@gmail.com>
2023-10-20 23:36:54 -0400

    remove before classes on ChatBubble.tsx

--- src/components/ChatBubble.tsx
+++ src/components/ChatBubble.tsx
@@ -9,13 +9,10 @@ export interface TextBubbleProps {
 const ChatBubble = ({ type, children }: TextBubbleProps) => {
   return (
     <div
-      className={classNames(
-        "m-1.5 flex w-fit max-w-full rounded-3xl px-4 py-2 text-xl shadow-md before:block before:content-['\x80']",
-        {
-          'bg-zinc-200 text-black dark:bg-zinc-700 dark:text-white': type === 'received',
-          'bg-blue-500 text-white dark:bg-blue-600': type === 'sent',
-        }
-      )}
+      className={classNames('m-1.5 flex w-fit max-w-full rounded-3xl px-4 py-2 text-xl shadow-md', {
+        'bg-zinc-200 text-black dark:bg-zinc-700 dark:text-white': type === 'received',
+        'bg-blue-500 text-white dark:bg-blue-600': type === 'sent',
+      })}
     >
       {children}
     </div>
