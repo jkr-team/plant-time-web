83dfd9d48cb1a6c2c612440fe42c23574186c683
Raza Mahmood <razammahmood@gmail.com>
2023-10-21 02:27:17 -0400

    adjust dark theme color bg in ChatForm

--- src/components/ChatForm.tsx
+++ src/components/ChatForm.tsx
@@ -58,7 +58,7 @@ function ChatFormBGImage() {
   return (
     <div
       className='absolute left-1/2 top-1/2 z-0 -translate-x-1/2 -translate-y-1/2 text-green-600 opacity-10
-   dark:text-zinc-500'
+   dark:text-green-300'
     >
       <Logo className='w-48' />
     </div>
