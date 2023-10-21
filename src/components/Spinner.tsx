19e91dcb03fd08f99541c6802abe65b55df7bb14
Raza Mahmood <razammahmood@gmail.com>
2023-10-20 23:24:34 -0400

    remove spinner component

--- src/components/Spinner.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Spinner({ hide = false, children }: { hide?: boolean; children?: React.ReactNode }) {
-  return <span className={`animate-spin transition-opacity ${hide ? 'opacity-0' : ''}`}>{children}</span>;
-}
