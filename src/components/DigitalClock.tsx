4fe974da16aec2edd1ac4ed98502e134b3d36ecc
Raza Mahmood <razammahmood@gmail.com>
2023-10-21 01:14:06 -0400

    only display hours and minutes in DigitalClock.tsx

--- src/components/DigitalClock.tsx
+++ src/components/DigitalClock.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { useTime } from '../hooks/useTime';
 
 export default function DigitalClock() {
-  const time = useTime('second');
+  const time = useTime('minute');
 
-  return <span suppressHydrationWarning={true}>{time.toLocaleTimeString()}</span>;
+  return (
+    <span suppressHydrationWarning={true}>
+      {time.toLocaleTimeString(undefined, { hour: 'numeric', minute: '2-digit' })}
+    </span>
+  );
 }
