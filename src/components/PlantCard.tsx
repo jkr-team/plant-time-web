92b26d2c31b167986776d15af59c021e6a915199
Raza Mahmood <razammahmood@gmail.com>
2023-10-25 00:52:02 -0400

    more consistent colors in PlantCard.tsx

--- src/components/PlantCard.tsx
+++ src/components/PlantCard.tsx
@@ -27,7 +27,7 @@ export default function PlantCard({ plant }: { plant: Plant }) {
           '[transform:rotateY(180deg)]': flipped,
         })}
       >
-        <div className='backface-hidden flex h-fit w-full flex-col overflow-hidden rounded-lg border-4 border-green-700 bg-white p-4 shadow-lg transition-[box-shadow] duration-500 hover:shadow-xl dark:border-green-200 dark:bg-black'>
+        <div className='backface-hidden flex h-fit w-full flex-col overflow-hidden rounded-lg border-4 border-green-700 bg-white p-4 shadow-lg transition-[box-shadow] duration-500 hover:shadow-xl dark:border-green-100 dark:bg-black'>
           <div className='flex w-full flex-col gap-1 dark:bg-green-100 bg-green-700 text-white dark:text-black p-4 text-center'>
             <span className='text-2xl font-bold'>{plant.name}</span>
             <span className='text-lg'>{plant.scientificName}</span>
@@ -36,7 +36,7 @@ export default function PlantCard({ plant }: { plant: Plant }) {
           <Image src={plant.image} alt={'Image of ' + plant.name} width={256} height={236} unoptimized />
         </div>
 
-        <div className='backface-hidden absolute left-0 top-0 flex h-full w-full flex-col overflow-auto rounded-lg border-4 border-green-700 bg-white p-4 text-black shadow-lg transition-[box-shadow] duration-500 [transform:rotateY(180deg)] hover:shadow-xl dark:border-green-200 dark:bg-zinc-800 dark:text-white'>
+        <div className='backface-hidden absolute left-0 top-0 flex h-full w-full flex-col overflow-auto rounded-lg border-4 border-green-700 bg-white p-4 text-black shadow-lg transition-[box-shadow] duration-500 [transform:rotateY(180deg)] hover:shadow-xl dark:border-green-100 dark:bg-zinc-800 dark:text-white'>
           <div className='flex w-full flex-col gap-2 p-4 relative min-h-full'>
             <div>
               <span className={'font-bold'}>Blooming Season:</span> {plant.season}
@@ -50,7 +50,7 @@ export default function PlantCard({ plant }: { plant: Plant }) {
             <div>
               <span className={'font-bold'}>Care Information:</span> {plant.careInformation}
             </div>
-            <div className='flex justify-center items-end w-full text-center flex-1 text-sm text-green-800 dark:text-green-300'>{plant.name}</div>
+            <div className='flex justify-center items-end w-full text-center flex-1 text-sm text-green-700 dark:text-green-100'>{plant.name}</div>
           </div>
         </div>
       </div>
