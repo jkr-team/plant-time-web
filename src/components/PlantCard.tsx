417623c850083f016165bc92b701d82ffc9f4b09
Raza Mahmood <razammahmood@gmail.com>
2023-10-25 00:44:33 -0400

    finished PlantCard flipping (need to make accessible)

--- src/components/PlantCard.tsx
+++ src/components/PlantCard.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import Image from 'next/image';
+import classNames from 'classnames';
 
 export type Plant = {
   name: string;
@@ -15,22 +16,42 @@ export default function PlantCard({ plant }: { plant: Plant }) {
   const [flipped, setFlipped] = React.useState(false);
 
   return (
-    <div className=' duration-700 animate-in fade-in slide-in-from-bottom'>
-      <div className='flex h-fit w-fit cursor-pointer overflow-hidden rounded-lg bg-transparent shadow-xl transition-[transform,box-shadow] duration-500 ease-in-out hover:scale-105 hover:shadow-2xl'>
-        <div className='flex w-full flex-col items-center justify-center'>
-          <div className='relative flex w-full items-center justify-center bg-black'>
-            <Image
-              src={plant.image}
-              alt={'Image of ' + plant.name}
-              width={256}
-              height={236}
-              unoptimized
-              className='z-10'
-            />
+    <div
+      className='h-fit w-72 cursor-pointer transition-transform duration-500 animate-in fade-in [perspective:1000px] hover:scale-105'
+      onClick={() => {
+        setFlipped(!flipped);
+      }}
+    >
+      <div
+        className={classNames('preserve-3d relative h-fit w-full transition-transform duration-700', {
+          '[transform:rotateY(180deg)]': flipped,
+        })}
+      >
+        <div className='backface-hidden flex h-fit w-full flex-col overflow-hidden rounded-lg border-4 border-green-700 bg-white p-4 shadow-lg transition-[box-shadow] duration-500 hover:shadow-xl dark:border-green-200 dark:bg-black'>
+          <div className='flex w-full flex-col gap-1 bg-green-100 p-4 text-center text-black'>
+            <span className='text-2xl font-bold'>{plant.name}</span>
+            <span className='text-lg'>{plant.scientificName}</span>
           </div>
-          <span className='w-full bg-green-50 p-4 text-center text-xl font-bold text-black dark:bg-green-200'>
-            {plant.name}
-          </span>
+
+          <Image src={plant.image} alt={'Image of ' + plant.name} width={256} height={236} unoptimized />
+        </div>
+
+        <div className='backface-hidden absolute left-0 top-0 flex h-full w-full flex-col overflow-auto rounded-lg border-4 border-green-700 bg-white p-4 text-black shadow-lg transition-[box-shadow] duration-500 [transform:rotateY(180deg)] hover:shadow-xl dark:border-green-200 dark:bg-zinc-800 dark:text-white'>
+          <div className='flex w-full flex-col gap-2 p-4 relative min-h-full'>
+            <div>
+              <span className={'font-bold'}>Blooming Season:</span> {plant.season}
+            </div>
+            <div>
+              <span className={'font-bold'}>Family:</span> {plant.family}
+            </div>
+            <div>
+              <span className={'font-bold'}>Height:</span> Up to {plant.heightPotential}cm
+            </div>
+            <div>
+              <span className={'font-bold'}>Care Information:</span> {plant.careInformation}
+            </div>
+            <div className='flex justify-center items-end w-full text-center flex-1 text-sm text-green-800 dark:text-green-300'>{plant.name}</div>
+          </div>
         </div>
       </div>
     </div>
