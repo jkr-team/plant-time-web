a48e70d2bafd8b95f4c07cb337834b39176e5ec5
Raza Mahmood <razammahmood@gmail.com>
2023-10-25 22:09:14 -0400

    update about size

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div className='flex w-96 flex-col items-center justify-center'>
-      <div className='flex w-full max-w-2xl flex-col items-center justify-center'>
-        <h1 className='text-center text-4xl font-bold'>About</h1>
-        <p className='text-center text-lg'>
-          Plant Time is a web app that helps you decide what to plant in your garden. It uses your location and
-          preferences to recommend plants that will thrive in your environment.
-        </p>
-        <p className='text-center text-lg'>Plant Time is a project by </p>
-      </div>
+    <div className='flex max-w-xl w-full flex-col items-center justify-center gap-4'>
+      <h1 className='text-center text-4xl font-bold'>About</h1>
+      <p className='text-center text-lg'>
+        Plant Time is a web app that helps you decide what to plant in your garden. It uses your location and
+        preferences to recommend plants that will thrive in your environment.
+      </p>
+      <p className='text-center text-lg'>Plant Time is a project by </p>
     </div>
   );
 }
