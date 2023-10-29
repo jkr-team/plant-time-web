b32aec61c6802978811ceb4e7477e655f151a7ab
Raza Mahmood <razammahmood@gmail.com>
2023-10-29 01:52:58 -0400

    update plant props

--- src/components/PlantCard.tsx
+++ src/components/PlantCard.tsx
@@ -4,12 +4,12 @@ import classNames from 'classnames';
 
 export type Plant = {
   name: string;
-  scientificName: string;
+  latinName: string;
   image: string;
-  season: string;
   family: string;
-  heightPotential: number;
-  careInformation: string;
+  height: number;
+  care: string;
+  color: string;
 };
 
 export default function PlantCard({ plant }: { plant: Plant }) {
@@ -37,26 +37,23 @@ export default function PlantCard({ plant }: { plant: Plant }) {
         <div className='backface-hidden flex h-fit w-full flex-col overflow-hidden rounded-lg border-4 border-green-800 bg-white p-4 shadow-lg transition-[box-shadow] duration-500 hover:shadow-xl dark:bg-zinc-900'>
           <div className='flex w-full flex-col gap-1 bg-green-800 p-4 text-center text-white'>
             <span className='text-2xl font-bold'>{plant.name}</span>
-            <span className='text-lg'>{plant.scientificName}</span>
+            <span className='text-lg'>{plant.latinName}</span>
           </div>
 
           <Image src={plant.image} alt={'Image of ' + plant.name} width={256} height={256} />
 
           <div className='flex w-full flex-col gap-1 bg-green-800 p-2 text-center text-white'>
-            <span className='text-lg'>{plant.family}</span>
+            <span className='text-lg'>{plant.family} Family</span>
           </div>
         </div>
 
         <div className='backface-hidden absolute left-0 top-0 flex h-full w-full flex-col overflow-auto rounded-lg border-4 border-green-800 bg-white p-4 text-black shadow-lg transition-[box-shadow] duration-500 [transform:rotateY(180deg)] hover:shadow-xl dark:border-green-700 dark:bg-zinc-800 dark:text-white'>
           <div className='relative flex min-h-full w-full flex-col gap-2 p-4'>
             <div>
-              <span className={'font-bold'}>Blooming Season:</span> {plant.season}
+              <span className={'font-bold'}>Height:</span> Up to {plant.height}cm
             </div>
             <div>
-              <span className={'font-bold'}>Height:</span> Up to {plant.heightPotential}cm
-            </div>
-            <div>
-              <span className={'font-bold'}>Care Information:</span> {plant.careInformation}
+              <span className={'font-bold'}>Care Tips:</span> {plant.care}
             </div>
             <div className='flex w-full flex-1 items-end justify-center text-center text-sm'>
               {plant.name}
--- src/components/PlantsGrid.tsx
+++ src/components/PlantsGrid.tsx
@@ -16,9 +16,9 @@ export default function PlantsGrid({ plants }: { plants: Plant[] }) {
 
   return (
     <div className='flex w-full flex-1 basis-0 flex-wrap justify-center gap-8 overflow-y-auto bg-white p-8 dark:bg-zinc-900'>
-      {(plants.length === 0 || loading) && <LoadingMessage message={'Loading plants'} />}
+      {(plants.length === 0 || loading) && <LoadingMessage message={'Computing Recommendations'} />}
 
-      {!loading && plants.map((plant) => <PlantCard plant={plant} key={plant.scientificName} />)}
+      {!loading && plants.map((plant) => <PlantCard plant={plant} key={plant.latinName + plant.name} />)}
     </div>
   );
 }
