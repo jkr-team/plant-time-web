4723c2917c70d6a03207b82b22d239fd80ddc512
Raza Mahmood <razammahmood@gmail.com>
2023-10-29 02:10:39 -0400

    update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,13 +2,29 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div className='flex max-w-xl w-full flex-col items-center justify-center gap-4 p-4'>
+    <div className='flex w-full max-w-xl flex-col items-center justify-center gap-8 p-4 text-lg'>
       <h1 className='text-center text-4xl font-bold'>About</h1>
       <p className='text-center text-lg'>
         Plant Time is a web app that helps you decide what to plant in your garden. It uses your location and
         preferences to recommend plants that will thrive in your environment.
       </p>
-      <p className='text-center text-lg'>Plant Time is a project by </p>
+
+      <div className='flex flex-col gap-2'>
+        <p className='text-center text-lg'>Plant Time was built by:</p>
+        <a href='https://github.com/khalil5754' className='text-center text-lg underline'>
+          Khalil Nayef (Back-end Developer)
+        </a>
+        <a href='#' className='text-center text-lg underline'>
+          Jocelyn Oja (Back-end Developer)
+        </a>
+        <a href='https://github.com/RazaMM' className='text-center text-lg underline'>
+          Raza Mahmood (Front-end Developer)
+        </a>
+      </div>
+
+      <a href='https://github.com/plant-time-team' className='rounded-xl bg-zinc-300 px-2 text-black'>
+        View the source code on Github!
+      </a>
     </div>
   );
 }
