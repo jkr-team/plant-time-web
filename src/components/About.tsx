b28464f076bc65eb83901e581d581de00446207c
Raza Mahmood <razammahmood@gmail.com>
2023-10-29 01:53:26 -0400

    padding on about oage

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div className='flex max-w-xl w-full flex-col items-center justify-center gap-4'>
+    <div className='flex max-w-xl w-full flex-col items-center justify-center gap-4 p-4'>
       <h1 className='text-center text-4xl font-bold'>About</h1>
       <p className='text-center text-lg'>
         Plant Time is a web app that helps you decide what to plant in your garden. It uses your location and
