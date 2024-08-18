3e4f04ccaa0cf3cdd92ec43f53448db41adc2e02
Raza Mahmood <razammahmood@gmail.com>
2024-08-18 01:04:59 -0400

    refactor chat component

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,32 +1,65 @@
 import React from 'react';
-import classNames from 'classnames';
-import ChatBubble, { TextBubbleProps } from './ChatBubble';
-import { flattenReactFragments } from '../utils/flattenReactFragments';
-import ChatTypingIndicator from './ChatTypingIndicator';
+import { twJoin, twMerge } from 'tailwind-merge';
 
-export interface ChatProps {
-  children: React.ReactNode;
+export interface ChatMessage {
+  content: string;
+  type: 'sent' | 'received';
   key?: React.Key;
 }
 
-const Chat = ({ key, children }: ChatProps) => {
-  const flattened = flattenReactFragments(children).filter(
-    (child) => React.isValidElement(child) && (child.type === ChatBubble || child.type === ChatTypingIndicator)
-  ) as React.ReactElement<TextBubbleProps>[];
+export interface ChatProps {
+  messages: ChatMessage[];
+  isTyping?: boolean;
+}
 
+export interface ChatBubbleProps {
+  children: React.ReactNode;
+  type: 'sent' | 'received';
+}
+
+export const ChatBubble = ({ type, children }: ChatBubbleProps) => {
+  return (
+    <div
+      className={twMerge(
+        'm-1.5 flex w-fit max-w-full rounded-3xl px-4 py-2 text-xl shadow-md',
+        type === 'received' && 'bg-zinc-200 text-black dark:bg-zinc-700 dark:text-white',
+        type === 'sent' && 'bg-blue-500 text-white dark:bg-blue-600'
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const ChatTypingIndicator = () => {
+  return (
+    <ChatBubble type={'received'}>
+      <div className='flex select-none items-center gap-1'>
+        <span className='animate-dim'>●</span>
+        <span className='animate-dim delay-100'>●</span>
+        <span className='animate-dim delay-300'>●</span>
+      </div>
+    </ChatBubble>
+  );
+};
+
+export const Chat = ({ messages, isTyping }: ChatProps) => {
   return (
     <div className='flex h-full w-full flex-col overflow-y-auto'>
-      {React.Children.map(flattened, (child, index) => (
+      {messages.map((message, index) => (
         <div
-          key={key ?? index}
-          className={classNames('flex w-3/4', {
-            'ml-auto justify-end': child.props.type === 'sent',
-            'mr-auto justify-start': child.props.type === 'received',
-          })}
+          key={message.key || index}
+          className={twJoin(
+            'flex w-3/4',
+            message.type === 'sent' && 'ml-auto justify-end',
+            message.type === 'received' && 'mr-auto justify-start'
+          )}
         >
-          {child}
+          <ChatBubble type={message.type}>{message.content}</ChatBubble>
         </div>
       ))}
+
+      {isTyping && <ChatTypingIndicator />}
     </div>
   );
 };
