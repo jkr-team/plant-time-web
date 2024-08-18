011fb8d0928eb1b8aa7982adb2c064104ab72fad
Raza Mahmood <razammahmood@gmail.com>
2024-08-18 02:11:36 -0400

    more container style improvements

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -3,8 +3,8 @@ import { twJoin } from 'tailwind-merge';
 import ThemeSwitch from './ThemeSwitch';
 
 export interface ContainerProps {
-  top: React.ReactNode;
-  children: React.ReactNode;
+  top?: React.ReactNode;
+  children?: React.ReactNode;
   wide?: boolean;
 }
 
@@ -14,8 +14,8 @@ export default function Container({ top, children, wide = false }: ContainerProp
       className={twJoin(
         'transition-aspect relative m-auto flex h-full w-full flex-col overflow-hidden shadow-2xl duration-700 dark:shadow-xl',
         wide
-          ? 'md:max-h-[min(95%,1000px)] md:max-w-[95%] md:rounded-3xl xl:aspect-[16/9] xl:h-[95vh] xl:w-auto'
-          : 'md:aspect-[9/16] md:h-[95vh] md:w-auto md:rounded-3xl'
+          ? 'md:max-h-[min(95%,1080px)] md:max-w-[95%] md:rounded-3xl xl:aspect-[16/9] xl:h-[95vh] xl:w-auto'
+          : 'md:aspect-[9/16] md:h-[95vh] md:max-h-[1080px] md:w-auto md:rounded-3xl'
       )}
     >
       <div className='z-20 flex items-center bg-zinc-100 px-4 py-2 text-2xl dark:bg-zinc-800'>
