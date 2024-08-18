4fb0257f990a07817445fba3a589a8db2147358f
Raza Mahmood <razammahmood@gmail.com>
2024-08-18 01:21:03 -0400

    remove imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import Form, { FormStep } from '../components/Form';
-import Container from '../components/Container';
-import classNames from 'classnames';
-import { City, geocode, getClosestCity, getLocation } from '../utils/geolocation';
-import Grid from '../components/Grid';
-import ThemeSwitch from '../components/ThemeSwitch';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleQuestion, faFaceSadTear } from '@fortawesome/free-solid-svg-icons';
-import DigitalClock from '../components/DigitalClock';
-import About from '../components/About';
-import { Plant } from '../components/Card';
-import Chat from '../components/Chat';
+import React from 'react';
 
 export default function Home() {
   return (
