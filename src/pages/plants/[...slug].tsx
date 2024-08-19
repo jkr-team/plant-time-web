54d2bf8df15891f547428cffc5cbc29a603f4089
Raza Mahmood <razammahmood@gmail.com>
2024-08-18 20:46:24 -0400

    add back button

--- src/pages/plants/[...slug].tsx
+++ src/pages/plants/[...slug].tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import { Container } from '../../components/Container';
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleLeft } from '@fortawesome/free-solid-svg-icons';
 
 export default function PlantsSubPages() {
-  return <Container></Container>;
+  return (
+    <Container
+      top={
+        <>
+          <Link className='text-zinc-500' href='/' aria-label='Go Back Home'>
+            <FontAwesomeIcon icon={faCircleLeft} />
+          </Link>
+        </>
+      }
+    ></Container>
+  );
 }
