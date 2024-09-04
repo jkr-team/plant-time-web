b355f3ddd4b2c496c28c62e9aca3ba373d09abb6
Raza Mahmood <razammahmood@gmail.com>
2024-09-03 23:56:27 -0400

    add spinner for layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,13 @@
 import { motion } from 'framer-motion';
 import React from 'react';
+import Spinner from '../img/spinner.svg';
 
 export type LayoutProps = {
   children?: React.ReactNode;
+  isLoading?: boolean;
 };
 
-export const Layout = ({ children }: LayoutProps) => {
+export const Layout = ({ children, isLoading }: LayoutProps) => {
   return (
     <motion.div
       initial={{ y: 300, opacity: 0 }}
@@ -18,6 +20,11 @@ export const Layout = ({ children }: LayoutProps) => {
       }}
       className='relative flex w-full flex-1 flex-col'
     >
+      {isLoading && (
+        <div className='absolute flex h-full w-full items-center justify-center bg-white text-green-500 dark:bg-zinc-900 [&>svg]:block [&>svg]:w-24'>
+          <Spinner />
+        </div>
+      )}
       {children}
     </motion.div>
   );
