ab310c5a3daaf7c794203d725718ee40777e1ab7
Raza Mahmood <razammahmood@gmail.com>
2024-09-03 23:56:37 -0400

    working on view all plants

--- src/pages/plants.tsx
+++ src/pages/plants.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Layout } from '../components/Layout';
+import { type Plant, PlantsGrid } from '../components/Plants';
+import useSWRInfinite from 'swr/infinite';
+
+const LIMIT = 2;
 
 export const getStaticProps = async () => {
   return {
@@ -9,8 +13,33 @@ export const getStaticProps = async () => {
   };
 };
 
+const fetcher = async (...args: any[]) => {
+  // @ts-expect-error
+  const res = await fetch(...args);
+
+  return res.json();
+};
+
+const getKey = (index: number, previous: Plant[]) => {
+  if (previous && !previous.length) {
+    return null;
+  }
+
+  return `/api/get-plants?page=${index}&limit=${LIMIT}`;
+};
+
 export default function PlantsPage() {
+  const { data, error, size, setSize } = useSWRInfinite(getKey, fetcher);
+
+  const isEmpty = data?.[0]?.length === 0;
+  const isDone = isEmpty || (data && data[data.length - 1]?.length < LIMIT);
+
+  const plants = data?.flat() as Plant[];
+
   return (
-    <Layout></Layout>
-  )
+    <Layout>
+      <PlantsGrid plants={plants ?? []} />
+      {!isDone && <button onClick={() => setSize(size + 1)}>Load More</button>}
+    </Layout>
+  );
 }
