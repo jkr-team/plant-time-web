93dd7a1f991757ab5fe27a0d16204891e86958a3
Raza Mahmood <razammahmood@gmail.com>
2024-09-03 21:11:02 -0400

    update home page look

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Apps } from '../components/Apps';
-import { LogoBackground } from '../components/LogoBackground';
+import LogoSVG from '../img/logo.svg';
 import { useTime } from '../utils/useTime';
 import { motion } from 'framer-motion';
 import { Layout } from '../components/Layout';
@@ -20,16 +20,19 @@ export default function HomePage() {
   return (
     <Layout>
       <div className='relative flex w-full flex-1 flex-col items-center px-4 py-10'>
-        <h1 className="text-4xl text-center">Welcome to Plant Time!</h1>
+        <div className='flex items-center justify-center gap-4 text-center text-4xl text-green-700'>
+          <LogoSVG className='block h-[2em]' />
+          <h1>Plant Time</h1>
+        </div>
 
-        <div className='flex-1 w-full max-h-[600px] my-auto z-20'>
+        <div className='z-20 my-auto max-h-[600px] w-full flex-1'>
           <Apps
             data={[
               {
                 title: 'Get Suggestions',
                 url: '/suggestions',
                 icon: (
-                  <div className='flex h-full w-full items-center justify-center text-6xl bg-green-200 dark:bg-green-950 '>
+                  <div className='flex h-full w-full items-center justify-center bg-green-200 text-6xl dark:bg-green-600'>
                     <FontAwesomeIcon icon={faWandMagicSparkles} />
                   </div>
                 ),
@@ -38,7 +41,7 @@ export default function HomePage() {
                 title: 'View Plants',
                 url: '/plants',
                 icon: (
-                  <div className='flex h-full w-full items-center justify-center text-6xl text-green-500 bg-sky-100 dark:bg-sky-950'>
+                  <div className='flex h-full w-full items-center justify-center bg-sky-100 text-6xl text-green-500 dark:bg-inherit'>
                     <FontAwesomeIcon icon={faLeaf} />
                   </div>
                 ),
